0354021a82bd091c3706953cbbc6798e8dd82b8e
clientesweb <alexisifran32@gmail.com>
2025-02-13 17:38:23 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
             ${About()}
             ${FeaturedProperties()}
             ${Services()}
-            ${ConstructionProgress()}
             ${OnlineMagazine()}
             ${Playlists()}
             ${Contact()}
