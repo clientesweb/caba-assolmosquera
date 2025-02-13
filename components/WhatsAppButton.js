e92c15817dd8a53ce19bf45156443329a67e8f67
clientesweb <alexisifran32@gmail.com>
2025-02-13 16:15:33 -0300

    Actualizar WhatsAppButton.js

--- components/WhatsAppButton.js
+++ components/WhatsAppButton.js
@@ -1,6 +1,6 @@
 function WhatsAppButton() {
   return `
-    <a href="https://wa.me/593987167782" target="_blank" rel="noopener noreferrer" class="fixed bottom-20 right-4 z-50 bg-green-500 text-white rounded-full p-3 shadow-lg hover:bg-green-600 transition-colors duration-300">
+    <a href="https://wa.me/5493546406871" target="_blank" rel="noopener noreferrer" class="fixed bottom-20 right-4 z-50 bg-green-500 text-white rounded-full p-3 shadow-lg hover:bg-green-600 transition-colors duration-300">
       <i class="fab fa-whatsapp text-3xl"></i>
     </a>
   `
